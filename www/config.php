b19df9070508bed06ff3f7993ec50943b6c8efee
Keiichi Lindley <digizn@gmail.com>
2015-06-17 09:57:15 -0600

    Switching redirect url in config

--- www/config.php
+++ www/config.php
@@ -5,7 +5,7 @@ $CLIENT_ID = '';
 $CLIENT_SECRET = '';
 
 /* VIMEO SETUP */
-$REDIRECT_URI = 'http://www.bomberwebdevelopment.com/vimeo/';
+$REDIRECT_URI = 'http://vimeo.giantdev.com/';
 $SCOPES = 'public private purchased create edit delete interact upload';
 $_SESSION['STATE'] = (isset($_SESSION['STATE'])) ? $_SESSION['STATE'] : substr(md5(rand()), 0, 21);
 
